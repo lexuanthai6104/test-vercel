5ad75a28c80f608ebd3872a3107c31c6041ae91a
Thaideptrai <lexuanthai6104@gmail.com>
2025-07-07 20:58:06 +0700

    update ÁCAO

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -17,6 +17,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Thêm CORS header
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === "POST") {
     try {
       const { salinity, pH, turbidity, temperature } = req.body;
